f61806cb5f508cae47fa1572d4e0daae2a2209db
Subhashini2610 <subhashini94@gmail.com>
2020-07-21 12:24:52 +0530

    Reduce, split and contains added

--- HigherOrderFunctions.playground/Contents.swift
+++ HigherOrderFunctions.playground/Contents.swift
@@ -4,6 +4,8 @@
  */
 //Source: https://www.appcoda.com/higher-order-functions-swift/
 
+//reduce, filter, compactMap reduce the size of existing array
+
 //Map on arrays, dictionaries
 let numbers = [2, 5, 3, 9, 15, 12, 8, 17, 20, 11]
 var info = [String: String]()
@@ -96,3 +98,84 @@ let results = testers.filter { (tester) -> Bool in
     tester.name.hasPrefix("J") && tester.age >= 30
 }
 print(results[0].name)
+
+//Reduce: for arrays
+let product = numbersArr.reduce(1) { (partialResult, element) -> Int in
+    partialResult * element
+}
+print(product)
+let prod = numbersArr.reduce(1, {$0 * $1})
+print(prod)
+let prodReduced = numbersArr.reduce(1, *)
+print(prodReduced)
+
+//Reduce: for dictionaries ($0 represents partial result)
+let friendsAndMoney = ["Alex": 150.00, "Tim": 62.50, "Alice": 79.80, "Jane": 102.00, "Bob": 94.20]
+let allMoney = friendsAndMoney.reduce(0, {$0 + $1.value})
+print(allMoney)
+
+//ForEach sample:
+numbers.forEach { (num) in
+    print(num.isMultiple(of: 2) ? "\(num) is even" : "\(num) is odd")
+}
+numbers.forEach({print($0.isMultiple(of: 2) ? "\($0) is even" : "\($0) is odd")})
+
+//Contains: always returns a Bool value
+class Staff {
+    enum Gender {
+        case male, female
+    }
+ 
+    var name: String
+    var gender: Gender
+    var age: Int
+ 
+    init(name: String, gender: Gender, age: Int) {
+        self.name = name
+        self.gender = gender
+        self.age = age
+    }
+}
+ 
+let staff = [Staff(name: "Nick", gender: .male, age: 37), Staff(name: "Julia", gender: .female, age: 29), Staff(name: "Tom", gender: .male, age: 41), Staff(name: "Tony", gender: .male, age: 45), Staff(name: "Emily", gender: .female, age: 42), Staff(name: "Irene", gender: .female, age: 30)]
+
+let staffOver40 = staff.contains { (staff) -> Bool in
+    return staff.age > 40
+}
+print(staffOver40)
+//Same goes for removeAll, removeAt
+
+//Sorted: the elements should conform to comparable
+//Intro_sort algorithm is used to perform the sorting.
+let sorted = numbers.sorted()
+print(sorted)
+
+//40 times
+let descArray = numbers.sorted { (elem1, elem2) -> Bool in
+    return elem1 > elem2
+}
+print(descArray)
+
+//41 times
+let descArray1 = numbers.sorted(by: {$0 > $1})
+print(descArray1)
+
+let descArray2 = numbers.sorted(by: >)
+print(descArray2)
+
+//Split: elements should conform to Collection protocol
+let message = "Hello world!!!"
+let result = message.split{ $0 == " " }
+print(result)
+let anotherMessage = "This message   is going to be broken in pieces!"
+let splitOnce = anotherMessage.split(maxSplits: 1, whereSeparator: { $0 == " " })
+let emptySequences = anotherMessage.split(omittingEmptySubsequences: false, whereSeparator: { $0 == " " })
+let messageSplit = message.split{ $0 == "." || $0 == "!"}
+print(splitOnce)
+print(emptySequences)
+print(messageSplit)
+
+//Note split always creates String.subsequence and not String
+//To create strings out of it:
+let strings = result.map { String($0) }
+print(strings)
