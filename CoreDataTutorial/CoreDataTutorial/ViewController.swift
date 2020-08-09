200bafe3a04ec3b4fdc80b8a7badb597732ca467
Subhashini2610 <subhashini94@gmail.com>
2020-08-09 18:46:07 +0530

    Core Data complete tutorial - setup with basic insert and fetch request. Persistence yet to be added. Hence, the data does not get saved after terminating the app.

---
+++ CoreDataTutorial/CoreDataTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTutorial
+//
+//  Created by Narayanaswamy, Subhashini (623) on 09/08/20.
+//  Copyright © 2020 Narayanaswamy, Subhashini (623). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
